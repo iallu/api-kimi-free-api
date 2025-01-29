4c1e51dfbf55dd3c5d11d55efd0e7f93eb911ffb
iallu <luamtblu+tuchuang@gmail.com>
2025-01-29 12:24:27 +0800

    one

--- src/vercel.ts
+++ src/vercel.ts
@@ -2,7 +2,12 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 import server from '@/lib/server';
 import routes from '@/api/routes/index';
 
+let isInitialized = false;
+
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  await server.attachRoutes(routes);
-  return server.app.callback()(req, res);
+    if (!isInitialized) {
+        await server.attachRoutes(routes);
+        isInitialized = true;
+    }
+    return server.app.callback()(req, res);
 } 
